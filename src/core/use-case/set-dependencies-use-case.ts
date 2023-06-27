7807dc106de0f0fa90762479297d4f44a900ac9a
Ran-j <ran-junior@hotmail.com>
2023-06-27 13:16:51 -0300

    feat: remove unused imports

--- src/core/use-case/set-dependencies-use-case.ts
+++ src/core/use-case/set-dependencies-use-case.ts
@@ -1,9 +1,6 @@
 import { HealthCheckRepository } from '../repository/health-check-repository';
 import { UseCaseBase } from './use-case';
 import { DependencyType } from '../entities/dependency-type';
-import { DependencyRunnerRepository } from '../repository/dependency-runner-repository';
-import { DependencyKindEnum } from '../entities/dependency-kind-enum';
-import { Container } from '../../container';
 
 export class SetDependenciesUseCase extends UseCaseBase<
   DependencyType[],
