a86d2632926b70d88ed2c01eb74f08a6ab28ec6a
Lucas Block <lucas.blockv@gmail.com>
2023-08-08 14:36:10 -0300

    fix: cpu usage + add tests

--- src/infra/repository/hash-map-health-check/hash-map-health-check.ts
+++ src/infra/repository/hash-map-health-check/hash-map-health-check.ts
@@ -146,15 +146,24 @@ export class HashMapHealthCheck implements HealthCheckRepository {
   }
 
   private getCpuUtilization(): number {
-    const cpuInfo = os.cpus();
-    const totalIdle = cpuInfo.reduce((acc, cpu) => acc + cpu.times.idle, 0);
-    const totalTick = cpuInfo.reduce(
-      (acc, cpu) =>
-        Object.values(cpu.times).reduce((tickAcc, tick) => tickAcc + tick, 0),
-      0,
-    );
-
-    return (1 - totalIdle / totalTick) * 100;
+    const cpus = os.cpus();
+    const numCPUs = cpus.length;
+  
+    let totalIdle = 0;
+    let totalTick = 0;
+  
+    for (const cpu of cpus) {
+      for (const type in cpu.times) {
+        totalTick += cpu.times[type];
+      }
+      totalIdle += cpu.times.idle;
+    }
+  
+    const idle = totalIdle / numCPUs;
+    const tick = totalTick / numCPUs;
+  
+    console.log((tick - idle) / tick);
+    return (tick - idle) / tick;
   }
 
   private getTotalMemory(): number {
---
+++ test/stubs/cpus.ts
@@ -0,0 +1,83 @@
+export const cpuUtilization = 0.05690717386670116;
+export const cpuInfoStub = [
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 3717,
+      times: { user: 752630, nice: 1280, sys: 348590, idle: 19231470, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 3720,
+      times: { user: 813640, nice: 250, sys: 320460, idle: 19219780, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 4345,
+      times: { user: 695820, nice: 400, sys: 269430, idle: 19197490, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 3718,
+      times: { user: 817860, nice: 220, sys: 395680, idle: 19124440, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 3717,
+      times: { user: 783720, nice: 350, sys: 313940, idle: 19251310, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 4636,
+      times: { user: 819600, nice: 240, sys: 374350, idle: 19154170, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 3717,
+      times: { user: 836160, nice: 120, sys: 363010, idle: 19148820, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 3719,
+      times: { user: 822200, nice: 1240, sys: 403480, idle: 19076480, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 3719,
+      times: { user: 869000, nice: 1860, sys: 409840, idle: 19034480, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 4647,
+      times: { user: 776500, nice: 930, sys: 313500, idle: 19142430, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 3741,
+      times: { user: 848120, nice: 170, sys: 319990, idle: 19188880, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 3718,
+      times: { user: 835760, nice: 220, sys: 376590, idle: 19103530, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 3719,
+      times: { user: 805030, nice: 660, sys: 366680, idle: 19169060, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 4650,
+      times: { user: 824500, nice: 150, sys: 395100, idle: 19109820, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 3719,
+      times: { user: 787950, nice: 110, sys: 261210, idle: 19309480, irq: 0 }
+    },
+    {
+      model: 'AMD Ryzen 7 5700X 8-Core Processor',
+      speed: 3719,
+      times: { user: 810560, nice: 2090, sys: 361180, idle: 19168460, irq: 0 }
+    }
+  ];
